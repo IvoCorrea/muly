613d773bd1af8bd957cdee8204fc0c6ef0f576c3
IvoCorrea <ivoaurelio30@gmail.com>
2026-09-12 17:23:00 -0300

    initial project

---
+++ src/main/java/com/muly/MulyApplication.java
@@ -0,0 +1,13 @@
+package com.muly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulyApplication.class, args);
+	}
+
+}
